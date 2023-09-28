ce8235fd72dbe893008bc284d0906bd5a80be68f
CleveTok3125 <97098535+CleveTok3125@users.noreply.github.com>
2023-09-28 22:03:40 +0700

    Updated to 4.1

---
+++ historykey.ps1
@@ -0,0 +1,1 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex "&{$((New-Object System.Net.WebClient).DownloadString('https://gist.github.com/MadeBaruna/1d75c1d37d19eca71591ec8a31178235/raw/getlink.ps1'))} global"

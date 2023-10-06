96c07f19db16ba17306c2a234ce064ff69b6b2f6
CleveTok3125 <97098535+CleveTok3125@users.noreply.github.com>
2023-10-06 17:24:38 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,20 +1,39 @@
+
 ## Description
-The script file automatically get the wish history link used for paimon.moe on Windows
+The application automatically get the wish history link used for paimon.moe on Windows
+## Use
+[Download](https://github.com/CleveTok3125/historykey/releases) and run `historykey.exe`
+#### To run every time the computer starts
+**Windows + R** to open Run
+```shell:startup```
+Paste the shortcut into the startup folder
 ## Guide
-1. [Install dependencies](#install-dependencies)
-2. [Set up configuration](#set-up-configuration)
+1. [Set up environment](#Set-up-environment)
+2. [Create executable file](#Create-executable-file)
 3. [About historykey.ps1](#about-historykey.ps1)
 4. [Run](#run)
-### Install dependencies <a name="install-dependencies"></a>
-`pip install -r requirements.txt`
-
-*Note: [historykey.exe](https://github.com/CleveTok3125/historykey/releases/) - executable version of `historykey.py` that can be run without installing dependencies.*
-### Set up configuration <a name="set-up-configuration"></a>
-Open historykey.ini with notepad or similar to configure
+5. [Close the application manually](#Close-the-application-manually)
+### Set up environment <a name="Set-up-environment"></a>
+```
+$ python -m venv historykey
+$ ".\historykey\Scripts\activate.bat"
+$ (historykey) pip install -r requirements.txt
+$ (historykey) ".\historykey\Scripts\activate.bat"
+```
+### Create executable file <a name="Create-executable-file"></a>
+```
+$ pip install pyinstaller
+$ pyinstaller --onefile --noconsole --clean --paths ".\historykey\Lib\site-packages" historykey.py
+$ move ".\dist\historykey.exe" ".\"
+$ del historykey.spec
+$ rmdir /q dist
+$ rmdir /s /q build
+$ rmdir /s /q historykey
+```
 ### About historykey.ps1 <a name="about-historykey.ps1"></a>
 - A third party powershell script from [MadeBaruna](https://gist.github.com/MadeBaruna/)
-- Read the `README.md` in [MadeBaruna](https://gist.github.com/MadeBaruna/) and find the code named `getlink.ps1` . Download or copy and save using the name `historykey.ps1` and place it in the folder containing `historykey.py`
-- By default `historykey.py` always uses `historykey.ps1`
+- Read the `README.md` in [MadeBaruna](https://gist.github.com/MadeBaruna/) and find the code named `getlink.ps1` . Download or copy and save using the name `historykey.ps1` and place it in the folder containing `historykey.exe`
+- By default `historykey.exe` always uses `historykey.ps1`
 ### Run <a name="run"></a>
 Supposed to run in historykey folder
 ```
@@ -26,3 +45,6 @@ or
 $ cd historykey
 $ historykey.exe
 ```
+*Note: the application will run in the background without any notification. To find out, look in the process manager. The application will stop when the wish history is obtained.*
+### Close the application manually<a name="Close-the-application-manually"></a>
+```$ taskkill /F /FI "Imagename eq historykey.exe" /T```
--- historykey.py
+++ historykey.py
@@ -1,24 +1,56 @@
-import time, subprocess, win32api, configparser
-config = configparser.ConfigParser()
-config.read('historykey.ini')
-limit = int(config.get('HISTORYKEY_SETTING', 'LIMIT'))
-count = 0
-if str(config.get('HISTORYKEY_SETTING', 'RUN_GENSHIN_IMPACT')).upper() == "TRUE":
-	subprocess.run(config.get('HISTORYKEY_SETTING', 'GENSHIN_PATH'))
-while True:
-	try:
-		powershell_command = 'Powershell.exe -executionpolicy remotesigned -File  historykey.ps1'
-		output = subprocess.check_output(['powershell', '-Command', powershell_command], shell=True, text=True)
-		if "Link copied to clipboard, paste it back to paimon.moe" in output:
-			win32api.MessageBox(0, output.split("\n")[2][0:50]+"..."+output.split("\n")[2][round(len(output.split("\n")[2])/2)-25:round(len(output.split("\n")[2])/2)+25]+"..."+output.split("\n")[2][-50:-1], "Link copied to clipboard")
-			break
-		elif "Cannot find the wish history url! Make sure to open the wish history first!" in output:
-			raise ValueError("Cannot find the wish history url! Make sure to open the wish history first!")
-		else:
+import time, subprocess, win32api, psutil, os, sys
+
+def wish_history():
+	powershell_command = 'Powershell.exe -executionpolicy remotesigned -File  historykey.ps1'
+	output = subprocess.check_output(['powershell', '-Command', powershell_command], shell=True, text=True)
+	if "Link copied to clipboard, paste it back to paimon.moe" in output:
+		win32api.MessageBox(0, output.split("\n")[2][0:50]+"..."+output.split("\n")[2][round(len(output.split("\n")[2])/2)-25:round(len(output.split("\n")[2])/2)+25]+"..."+output.split("\n")[2][-50:-1], "Link copied to clipboard")
+		return True
+	elif "Cannot find the wish history url! Make sure to open the wish history first!" in output:
+		raise ValueError("Cannot find the wish history url! Make sure to open the wish history first!")
+		return False
+	else:
+		return False
+
+def is_true_fn():
+	cwf = os.path.basename(sys.argv[0])
+	if (cwf == 'historykey.py') or (cwf == 'historykey.exe'):
+		return True
+	return False
+
+def is_running():
+	processes = psutil.process_iter()
+	list_proc = []
+	for p in processes:
+		if p.name() == 'historykey.exe':
+			list_proc.append(p.name())
+	num_proc = len(list_proc)
+	if (num_proc == 1) or ((num_proc % 2 == 0) and (num_proc > 2)):
+		return True
+	else:
+		return False
+
+def is_depen_exst():
+	return os.path.exists("historykey.ps1")
+
+if is_true_fn() == False:
+	win32api.MessageBox(0, "Unable to locate executable file", "Error")
+elif is_running():
+	win32api.MessageBox(0, "Another Instance Is Already Running.", "Error")
+elif is_depen_exst() == False:
+	win32api.MessageBox(0, 'Could not find dependency file "historykey.ps1"', "Error")
+else:
+	while True:
+		try:
+			if wish_history():
+				break
+			else:
+				processes = psutil.pids()
+				for process in processes:
+					name = psutil.Process(process).name()
+					if "GenshinImpact" in name:
+						if wish_history():
+							break
+		except:
 			None
-	except:
-		count += 1
-		if count >= limit:
-			win32api.MessageBox(0, "Can't find link after "+str(limit)+" attempt(s)", "Error")
-			break
-	time.sleep(1)
+		time.sleep(1)

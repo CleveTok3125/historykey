a6fe02fad249bafe470a408dbdae4686a170b4ee
CleveTok3125 <97098535+CleveTok3125@users.noreply.github.com>
2024-01-05 10:27:55 +0700

    Add files via upload

--- historykey.py
+++ historykey.py
@@ -4,7 +4,10 @@ def wish_history():
 	powershell_command = 'Powershell.exe -executionpolicy remotesigned -File  historykey.ps1'
 	output = subprocess.check_output(['powershell', '-Command', powershell_command], shell=True, text=True)
 	if "Link copied to clipboard, paste it back to paimon.moe" in output:
-		win32api.MessageBox(0, output.split("\n")[2][0:50]+"..."+output.split("\n")[2][round(len(output.split("\n")[2])/2)-25:round(len(output.split("\n")[2])/2)+25]+"..."+output.split("\n")[2][-50:-1], "Link copied to clipboard")
+		for i in output.split("\n"):
+			if 'http' in i:
+				output = i
+		win32api.MessageBox(0, output[0:50]+"..."+output[round(len(output)/2)-25:round(len(output)/2)+25]+"..."+output[-50:-1], "Link copied to clipboard")
 		return True
 	elif "Cannot find the wish history url! Make sure to open the wish history first!" in output:
 		raise ValueError("Cannot find the wish history url! Make sure to open the wish history first!")
